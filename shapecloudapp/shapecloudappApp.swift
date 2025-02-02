15c66f4dc820dce3c97b92f5e784236da3af1da4
miguel <miguel.gutierrezt@pucp.pe>
2025-02-02 17:26:48 -0500

    Initial Commit

---
+++ shapecloudapp/shapecloudappApp.swift
@@ -0,0 +1,17 @@
+//
+//  shapecloudappApp.swift
+//  shapecloudapp
+//
+//  Created by Miguel Gutiérrez on 2/02/25.
+//
+
+import SwiftUI
+
+@main
+struct shapecloudappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ shapecloudappTests/shapecloudappTests.swift
@@ -0,0 +1,17 @@
+//
+//  shapecloudappTests.swift
+//  shapecloudappTests
+//
+//  Created by Miguel Gutiérrez on 2/02/25.
+//
+
+import Testing
+@testable import shapecloudapp
+
+struct shapecloudappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
